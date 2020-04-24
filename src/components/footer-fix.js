549a007019952a431e08c4536f1e67d805261187
rkhaidir <rkhaidir085@gmail.com>
2020-04-24 22:27:05 +0800

    Update Footer

--- src/components/footer-fix.js
+++ src/components/footer-fix.js
@@ -24,11 +24,15 @@ class Footerfix extends HTMLElement {
                 flex: 1;
                 padding: 25px;
             }
-            .footer-content .about h1 {
+            .footer-content .about a {
+                color: #fff;
+                font-weight: 200;
+            }
+            .footer-content .profil h2 {
                 color: #e9e9e9;
                 font-weight: 200;
             }
-            .footer-content .about a {
+            .footer-content .profil a {
                 color: #fff;
                 font-weight: 200;
             }
@@ -43,15 +47,30 @@ class Footerfix extends HTMLElement {
                 left: 0;
                 padding-top: 20px;
             }
+
+            @media only screen and (max-width: 800px) {
+                .footer-content {
+                    height: auto;
+                    flex-direction: column;
+                }
+                .footer-content .profil {
+                    padding-bottom: 80px;
+                }
+            }
             </style>
 
             <div class="footer-content">
                 <div class="footer-section about">
                     <h1>Kawal Corona</h1>
                     <p>
-                        Kawal Corona merupakan sebuah website untuk memantau perkembangan kasus corona di Indonesia.<br><br>
+                        Kawal Corona merupakan sebuah website untuk memantau perkembangan kasus corona di Indonesia. Website ini mengambil data dari <a href="https://kawalcorona.com/api/">kawalcorona.com/api</a>.<br><br>
                         <a href="https://github.com/rkhaidir/submission-fundamental-frontend-web">Sorce Code</a>
                     </p>
+                </div>
+                <div class="footer-section profil">
+                    <h2>Profil Pembuat</h2>
+                    <p>Rakhmat Khaidir</p>
+                    <p><a href="https://github.com/rkhaidir">Github</a> | <a href="https://web.facebook.com/rakhmat.khaidir.90">Facebook</a> | <a href="https://twitter.com/rkhaidir16">Twitter</a></p>
                 </div>
             </div>
             <div class="footer-bottom">
